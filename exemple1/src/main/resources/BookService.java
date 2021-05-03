440c422466952cbc6369bee07da75104e5f51e9a
Petusin <petusin1970@gmail.com>
2021-05-03 18:41:03 +0200

    commit server 8081

---
+++ exemple1/src/main/resources/BookService.java
@@ -0,0 +1,16 @@
+
+
+import org.springframework.web.client.RestTemplate;
+
+public class BookService {
+	
+	RestTemplate bookTemplate = new RestTemplate();
+	
+	
+	public Iterable <Book> queryBooks() {
+		
+		
+		return null;
+		
+	}
+}
